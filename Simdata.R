06431921109e858d0d6f51e5c578bb2049a22cc8
mcl868 <thomas.maltesen@protonmail.com>
2020-08-17 10:02:52 +0200

    Create Simdata.R

---
+++ Simdata.R
@@ -0,0 +1,14 @@
+age<-sample(c(18:64),200,TRUE)
+gender<-sample(c("female","male"),200,TRUE)
+education<-sample(c("lower","mid","high"),200,TRUE)
+area<-sample(c("north","south","west","east"),200,TRUE)
+
+y<-age+
+   1*(gender=="male")+
+   1*(education=="mid")+2*(education=="high")+
+   1*(area=="north")+2*(area=="west")+3*(area=="east")+
+   rnorm(200)
+database<-data.frame("age"=age,"gender"=gender,"education"=education,"area"=area,"y"=y)
+rm(list=c("age","gender","education","area","y"))
+
+write.csv(database,"~/GitHub/dataforprojects/ArtifDataFrame.csv", row.names = FALSE)
